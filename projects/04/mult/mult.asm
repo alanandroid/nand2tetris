e2fc4cb53affa3ce2d9b2803e075b4ec4a497029
Alan Ray Tatro <alan.tatro@nike.com>
2017-03-22 16:26:44 -0700

    broken multiplier

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,47 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  @R2
+  M=0
+
+  @R1
+  D=M
+  @END
+  D;JEQ
+
+  @R0
+  D=M
+  @END
+  D;JEQ
+
+  // make a counter, set it equal to 0
+  @counter
+  M=0
+  // make a product variable, set it equal to R0
+  @R0
+  D=M
+  @product
+  M=D
+
+// loop addition to produce multiplication
+(LOOP)
+  // add R0 to product
+  @R0
+  D=M
+  @product
+  M=D+M
+  // increment counter
+  @counter
+  M=M+1
+  // check loop condition (counter <= R1)
+  D=M
+  @R1
+  D=D-M
+  @END
+  D;JLT
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
