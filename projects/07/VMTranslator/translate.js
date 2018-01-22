3b546d469f8e636ece61b266556f940c84a8ebfb
Alan Ray Ampersand <alanray@andperse.design>
2018-01-21 20:09:40 -0800

    push/pop local coded, not tested'

--- projects/07/VMTranslator/translate.js
+++ projects/07/VMTranslator/translate.js
@@ -1,7 +1,7 @@
 class CompilationError extends Error {
-  constructor(line) {
-    super('ERROR: Compilation error'
-      + 'unable to parse line "' + line + '"');
+  constructor(line, lineNumber) {
+    super('Error at line number ' + lineNumber + '.\n'
+      + 'Unable to parse "' + line + '"');
     this.name = "CompilationError";
   }
 }
@@ -59,13 +59,13 @@ if (process.argv.length < 3) {
     // create, or clear content in the output file.
     fs.writeFileSync(output, '');
 
-    fs.readFileSync(input).toString().split('\n').forEach(function (line) {
-      fs.appendFileSync(output, translateLine(line));
+    fs.readFileSync(input).toString().split('\n').forEach(function (line, lineNumber) {
+      fs.appendFileSync(output, translateLine(line, lineNumber + 1));
     });
   }
 }
 
-function translateLine(line) {
+function translateLine(line, lineNumber) {
   line = line.toString();
   line = removeExtraWhiteSpaceFrom(line);
   // if the string is empty, return an empty line
@@ -76,41 +76,52 @@ function translateLine(line) {
   let translatedLine;
 
   switch(words[0].toUpperCase()) {
+
     case 'POP':
       translatedLine = translatePOP(words);
       break;
+
     case 'PUSH':
       translatedLine = translatePUSH(words);
       break;
+
     case 'ADD':
       translatedLine = translateADD(words);
       break;
+
     case 'SUB':
       translatedLine = translateSUB(words);
       break;
+
     case 'NEG':
       translatedLine = translateNEG(words);
       break;
+
     case 'EQ':
       translatedLine = translateEQ(words);
       break;
+
     case 'GT':
       translatedLine = translateGT(words);
       break;
+
     case 'LT':
       translatedLine = translateLT(words);
       break;
+
     case 'AND':
       translatedLine = translateAND(words);
       break;
+
     case 'OR':
       translatedLine = translateOR(words);
       break;
+
     case 'NOT':
       translatedLine = translateNOT(words);
       break;
     default:
-      throw new CompilationError(line);
+      throw new CompilationError(line, lineNumber);
   }
 
   const completeStatement =
@@ -121,13 +132,168 @@ function translateLine(line) {
 }
 
 function translatePOP(words) {
+  const memoryAddress = words[1];
+  const memoryAddress = parseInt(words[2]);
+
+  switch (memorySegment) {
+
+    case 'CONSTANT':
+      return 'CONST';
+
+    case 'LOCAL':
+      return '' +
+      // addr = LCL+memoryAddress
+      // look at the memory address in order to get it's value into memory
+      '@' + memoryAddress + '\n' +
+      // store that value in the data register
+      'D=A' + '\n' +
+      // look at LCL in memory
+      '@LCL' + '\n' +
+      // add LCL to the memory address
+      'D=D+A' + '\n' +
+      // create a new variable
+      '@addr'
+      // store LCL+memoryAddress in it
+      'M=D'
+      // SP--;
+      '@SP' + '\n' +
+      'M=M-1' + '\n' +
+      //*addr = *SP
+      // @SP should be here, but we're already looking at it
+      // *SP - point at the memory address stored in @SP
+      'A=M' + '\n' +
+      // store whatever is there in the data register
+      'D=M' + '\n' +
+      // point at our variable created above, which contains LCL+memoryAddress
+      '@addr' + '\n' +
+      // point the address register at LCL+memoryAddress
+      'A=M' + '\n' +
+      // and put the contents of the data register (the contents of *SP) there
+      'M=D' + '\n';
+
+    case 'ARGUMENT':
+      return 'ARG';
+
+    case 'THIS':
+      return 'THIS';
+
+    case 'THAT':
+      return 'THAT';
+
+    case 'CONSTANT':
+      // TODO fixxxxx
+      return 'CONST';
+
+    case 'STATIC':
+      return 'STATIC';
+
+    case 'POINTER':
+      let thisOrThat;
+      if (memoryAddress === 0) {
+        thisOrThat = 'THIS';
+      } else if (memoryAddress === 1) {
+        thisOrThat = 'THAT';
+      } else {
+        // TODO make this less bad
+        throw new Error('A nondesript error occurred.');
+      }
+      return 'SP--' + '\n' + thisOrThat + ' = *SP';
+
+    case 'TEMP':
+      return 'TEMP';
+
+    default:
+      return '';
+  }
+
   return 'POP';
 }
 
 function translatePUSH(words) {
-  if (words.length < 2) {
-    throw new CompilationError("hi", 1)
+  const memorySegment = words[1];
+  const memoryAddress = parseInt(words[2]);
+
+  switch (memorySegment) {
+
+    case 'CONSTANT':
+      return 'CONST';
+
+    case 'LOCAL':
+      return '' +
+      // ------------------------
+      // *SP = *addr
+      // ------------------------
+      // look at the memory address in order to get it's value into memory
+      '@' + memoryAddress + '\n' +
+      // store that value in the data register
+      'D=A' + '\n' +
+      // look at LCL in memory
+      '@LCL' + '\n' +
+      // add LCL to the memory address
+
+      // was 'D=D+A' + '\n' +
+      // point the address register at LCL+memoryAddress
+      'A=D+A' + '\n' +
+        // TODO delete indented?
+          // create a new variable
+          // '@addr'
+          // store LCL+memoryAddress in it
+          // 'M=D'
+          // -----------
+          // *SP = *addr
+          // -----------
+          // '@addr' + '\n' +
+          // *SP - point at the memory address stored in @SP
+          // 'A=M' + '\n' +
+      // store whatever is there in the data register
+      'D=M' + '\n' +
+      // point at our variable created above, which contains LCL+memoryAddress
+      '@SP' + '\n' +
+      // point the address register at LCL+memoryAddress
+      'A=M' + '\n' +
+      // and put the contents of the data register (the contents of *SP) there
+      'M=D' + '\n';
+      // -----
+      // SP++;
+      // -----
+      '@SP' + '\n' +
+      'M=M+1' + '\n' +
+
+    case 'ARGUMENT':
+      return 'ARG';
+
+    case 'THIS':
+      return 'THIS';
+
+    case 'THAT':
+      return 'THAT';
+
+    case 'CONSTANT':
+      // TODO fixxxxx
+      return 'CONST';
+
+    case 'STATIC':
+      return 'STATIC';
+
+    case 'POINTER':
+      let thisOrThat;
+      if (memoryAddress === 0) {
+        thisOrThat = 'THIS';
+      } else if (memoryAddress === 1) {
+        thisOrThat = 'THAT';
+      } else {
+        // TODO make this less bad
+        throw new Error('A nondesript error occurred.');
+      }
+      return '*SP = ' + thisOrThat + '\n' + 'SP++';
+
+    case 'TEMP':
+      return 'TEMP';
+
+    default:
+      return '';
   }
+
   return 'PUSH';
 }
 
@@ -170,7 +336,6 @@ function translateNOT(words) {
 function removeExtraWhiteSpaceFrom(string) {
   return string.replace(/\s+/g, ' ').trim();
 }
-
 
 /*
 add, sub, neg, eq, gt, lt, and, or not
