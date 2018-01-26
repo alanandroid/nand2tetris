39779839581658279f8bcd25e850774724ddba60
Alan Ray <alan.ampersand@nike.com>
2018-01-26 12:25:19 -0800

    comments

--- projects/07/VMTranslator/pushFunctions.js
+++ projects/07/VMTranslator/pushFunctions.js
@@ -1,6 +1,19 @@
 /**
  * File Overview:
  *
+ * This file contains functions whose purpose is to generate symbolic assembly
+ * for the PUSH opertaions in the HACK VM code. Descriptions found within
+ * heavily leverage pseudo symbolic assembly code.
+ *
+ * You can find a thorough explanation of the symbolic assemly used here in
+ * Chapter 4 of The Elements of Computing Systems by Noam Nisan and Shimon
+ * Schocken, which is available for free on the internet at the course website,
+ * found at http://www.nand2tetris.org/ as of this writing.
+ *
+ * Note: The pseudo-syntax *{address} is not dicussed until Chapter 7. It is
+ * used to denote the data store in the memory address found in the register at
+ * {address}. For example, if the value at the top of your stack is 5, *SP is
+ * equal to the value found in RAM[5].
  */
 const { getFileName, parseMemorySegment, parseTHISorTHAT } = require('./utilFunctions');
 
